7f17888e4fa73daaabd64d043462f6d53f9649c7
TrendoraHub1 <mohsinsattar005@gmail.com>
2026-07-29 03:55:03 -0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,14 @@
+// =====================================
+// TrendoraHub Admin Panel
+// Part 1
+// =====================================
 
+const productForm = document.querySelector(".product-form");
+
+productForm.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Product Publish Button Working Successfully!");
+
+});
