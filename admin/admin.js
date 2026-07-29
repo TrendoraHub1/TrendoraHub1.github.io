9d081f545d17fe11fb4981cc2e1269102175e7c9
TrendoraHub1 <mohsinsattar005@gmail.com>
2026-07-29 03:58:52 -0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -12,3 +12,36 @@ productForm.addEventListener("submit", function(event){
     alert("Product Publish Button Working Successfully!");
 
 });
+// =====================================
+// Read Form Data
+// =====================================
+
+const publishButton = document.querySelector(".product-form button");
+
+publishButton.addEventListener("click", function(){
+
+    const productName = document.querySelector('input[type="text"]').value;
+
+    const productPrice = document.querySelectorAll('input[type="text"]')[1].value;
+
+    const affiliateLink = document.querySelector('input[type="url"]').value;
+
+    const mainCategory = document.querySelectorAll("select")[0].value;
+
+    const collectionCategory = document.querySelectorAll("select")[1].value;
+
+    const description = document.querySelector("textarea").value;
+
+    console.log("Product Name:", productName);
+
+    console.log("Price:", productPrice);
+
+    console.log("Affiliate Link:", affiliateLink);
+
+    console.log("Main Category:", mainCategory);
+
+    console.log("Collection Category:", collectionCategory);
+
+    console.log("Description:", description);
+
+});
