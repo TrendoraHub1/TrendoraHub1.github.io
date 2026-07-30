c869699ecbc0ebfb549fcc3c37487b9b7bc191af
TrendoraHub1 <mohsinsattar005@gmail.com>
2026-07-30 10:20:06 -0700

    Update fashion.js

--- fashion.js
+++ fashion.js
@@ -414,3 +414,54 @@ window.addEventListener("load",()=>{
 
 
 });
+
+// =====================================
+// Load Fashion Products
+// =====================================
+
+function loadFashionProducts(){
+
+    const productGrid = document.querySelector(".product-grid");
+
+    if(!productGrid) return;
+
+    const products =
+    JSON.parse(localStorage.getItem("products")) || [];
+
+    const fashionProducts = products.filter(function(product){
+
+        return product.mainCategory === "Fashion";
+
+    });
+
+    productGrid.innerHTML = "";
+
+    fashionProducts.forEach(function(product){
+
+        productGrid.innerHTML += `
+
+        <div class="product-card">
+
+            <img src="${product.image}" alt="${product.name}">
+
+            <h3>${product.name}</h3>
+
+            <p>${product.description}</p>
+
+            <a
+            href="store-system/store-product.html?id=${product.id}"
+            class="buy-btn">
+
+            View Product
+
+            </a>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+loadFashionProducts();
